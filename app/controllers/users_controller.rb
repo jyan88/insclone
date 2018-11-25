3ede4773abf59dc066bbf33b67eee2412a55c980
hiroki <hiroki7255@yahoo.co.jp>
2018-11-25 05:00:26 +0000

    制御３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update, :show, :new]
   
   def new
     @user = User.new
